b154a6d28ac5e0652b297b29c5d1267e88d85d07
sebastiannarvaez23 <narvaezsebas8@gmail.com>
2024-09-15 18:45:09 -0500

    Ajuste del controlador

--- src/microservices/users/infrastructure/api/users.controller.ts
+++ src/microservices/users/infrastructure/api/users.controller.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 
 import { EncryptionService } from "../../services/encryption.service";
 import { PersonManagement } from "../../application/use-cases/person-management";
--- src/microservices/users/infrastructure/api/users.middleware.ts
+++ src/microservices/users/infrastructure/api/users.middleware.ts
@@ -1,4 +1,3 @@
-import { NextFunction, Request, Response } from "express";
 import { PersonValidator } from "../../application/validations/person.validator";
 import { validationMiddleware } from "../../../../api-gateway/middlewares/validation.middleware";
 
@@ -6,13 +5,6 @@ const userValidator = new PersonValidator();
 
 export class UsersMiddleware {
     constructor() { }
-    async validateAdd(req: Request, res: Response, next: NextFunction) {
-        try {
-            next();
-        } catch (error) {
-            console.error('Validation error:', error);
-            res.status(400).json({ message: 'Validation failed' });
-        }
-    }
+    validateAdd = validationMiddleware(userValidator);
     validateEdit = validationMiddleware(userValidator);
 }
