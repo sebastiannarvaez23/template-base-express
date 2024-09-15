314b09bc7ef490f3459c0c6223aa704514c3ef44
sebastiannarvaez23 <narvaezsebas8@gmail.com>
2024-09-15 18:49:10 -0500

    Implementación del metodo para encriptación

--- src/microservices/users/services/encryption.service.ts
+++ src/microservices/users/services/encryption.service.ts
@@ -1,10 +1,29 @@
 import * as CryptoJS from 'crypto-js';
 
 export class EncryptionService {
-    private readonly key: string = 'mySecretKey1234567890';
+
+    private readonly key!: string;
     private readonly keySize = 256;
     private readonly ivSize = 128;
     private readonly iterations = 1000;
+
+    constructor() {
+        this.key = process.env.SECRET_KEY!;
+    }
+
+    encrypt(plainText: string): string {
+        const salt = CryptoJS.lib.WordArray.random(16);
+        const iv = CryptoJS.lib.WordArray.random(16);
+
+        const key = CryptoJS.PBKDF2(this.key, salt, {
+            keySize: this.keySize / 32,
+            iterations: this.iterations
+        });
+        const encrypted = CryptoJS.AES.encrypt(plainText, key, { iv: iv }).toString();
+        const saltHex = salt.toString(CryptoJS.enc.Hex);
+        const ivHex = iv.toString(CryptoJS.enc.Hex);
+        return saltHex + ivHex + encrypted;
+    }
 
     decrypt(encryptedText: string): string {
         const salt = CryptoJS.enc.Hex.parse(encryptedText.substr(0, 32));
@@ -15,11 +34,9 @@ export class EncryptionService {
             keySize: this.keySize / 32,
             iterations: this.iterations
         });
-
         const decrypted = CryptoJS.AES.decrypt(encrypted, key, {
             iv: iv
         });
-
         return decrypted.toString(CryptoJS.enc.Utf8);
     }
 }
